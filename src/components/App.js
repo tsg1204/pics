8af180351b386610477356a74b8b1b6f597358d7
Tatiana Grigorieva <tsg1204@gmail.com>
2020-11-28 10:46:31 -0500

    SearchBar comp added

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SearchBar from './SearchBar';
+import ImageList from './ImageList';
+
+const App = () => {
+    return (
+        <>
+            <SearchBar />
+            <ImageList />
+        </>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+
+import App from './components/App';
 
 
-ReactDOM.render( <div>Test</div>,document.getElementById('root'));
+
+ReactDOM.render( <App />, document.getElementById('root'));
